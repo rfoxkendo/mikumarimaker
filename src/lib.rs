1e4e5ebdebdcc04842b6a7602f8594e54f63d8da
Ron Fox <fox@frib.msu.edu>
2026-01-26 09:35:18 -0500

    Initial coding.
    Wrote the code for high resolution leading and trailing tdc hits.

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,24 @@ pub mod mikumari_format {
     struct Delimeter2 {
         delimeter : u64
     }
-    // If I read the docs correctly, there's no InputThrottleingT1 only 
-    // T2 for whatever reason.
+    // Sample does not show the throttles so skip to the chase:
+    // Not sure how software tells the difference between high and 
+    // low resolution as I don't see separate data types for them.
+    // Assumption:  Time over threshold will only be present in the trailing
+    // time as the TOT is from leading to trailing edge(?).
+    struct HRTDCLeading {
+        leading : u64
+    }
+    struct HRTDCTrailing {
+        trailing : u64
+    }
 
-
-
+    struct LRTDCLeading {
+        leading : u64
+    }
+    struct LRTDCTrailing {
+        trailing : u64
+    }
     // TODO: range check the inputs as they're not full sized.
     impl Delimeter1 {
         pub fn new(time_offset : u16, frame_number: u32) -> Delimeter1 {
@@ -56,6 +69,61 @@ pub mod mikumari_format {
         pub fn get (&self) -> u64 {
             self.delimeter
         }      
+    }
+    impl HRTDCLeading {
+        pub fn new(chan : u8, tot : u32, time : u32) -> HRTDCLeading {
+            let mut value = (TDC_LeadingData as u64) << 58;
+            value |= (chan as u64) << 51;
+            value |= (tot as u64)     << 29;
+            value |= time as u64;
+
+            HRTDCLeading {
+                leading : value
+            }
+        }
+        // Getters:
+
+        pub fn channel(&self) -> u8 {
+            ((self.leading  >> 51) & 0x7f) as u8
+        }
+        pub fn TOT(&self) -> u32 {
+            ((self.leading >> 29) & 0x3fffff) as u32
+        }
+        pub fn Time(&self) -> u32 {
+            (self.leading & 0x1FFFFFFF) as u32
+        }
+    }
+    // In fact, other than the data type fie.d, 
+    // this is just like the leading edge so we do do some dirty stuff.
+
+    impl HRTDCTrailing {
+        pub fn new(chan : u8, tot : u32, time : u32) -> HRTDCTrailing {
+            let leading = HRTDCLeading::new(chan, tot, time);
+            // maks off the data type and replace it with 0x34
+
+            let mut data = leading.leading;
+            data &= !(TDC_LeadingData as u64) << 58;
+            data |= (TDC_TrailingData as u64)<< 58;
+
+            HRTDCTrailing {
+                trailing : data
+            }
+        }
+        // Here's where the dirt is, we use the LE functions.
+        // The dirt we use in this method gets used in all others as well.
+        pub fn channel(&self) -> u8 {
+            let leading = HRTDCLeading { leading: self.trailing};  // the dirt:
+
+            leading.channel()
+        }
+        pub fn TOT(&self) -> u32 {
+            let leading = HRTDCLeading { leading: self.trailing};
+            leading.TOT()
+        }
+        pub fn Time(&self) -> u32 {
+            let leading = HRTDCLeading { leading: self.trailing};
+            leading.Time()
+        }
     }
     #[cfg(test)]
     mod delim1test {
