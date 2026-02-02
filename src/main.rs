4a49ccb388158bedca67490e6f3c027dbdc24941
Ron Fox <fox@frib.msu.edu>
2026-02-02 08:36:18 -0500

    Handle arguments to get the input file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use mikumarimaker::mikumari_format;
-
+use std::env::args;
+use std::process::exit;
 fn main() {
-    
+    let argv : Vec<String> = args().collect();
+    if argv.len() != 2 {
+        eprintln!("This program rerquires the name of a mmikumari input file ");
+        exit(-1);
+    }
+    let fname = argv[0].clone();
 }
